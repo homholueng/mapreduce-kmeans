98ea2cfa84f4b1c7f74032cedbce097d5105f26f
AusarMon <zxc294811099@qq.com>
2017-06-15 20:24:59 +0800

    懒得写了

--- webpage-mapreduce/src/main/java/mapred/WordPercentCounter.java
+++ webpage-mapreduce/src/main/java/mapred/WordPercentCounter.java
@@ -1,10 +1,18 @@
 package mapred;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -16,18 +24,19 @@ import static mapred.config.Constants.SEPERATOR;
 /**
  * Created by HL on 14/06/2017.
  */
-public class Mapred2 {
+public class WordPercentCounter {
 
-    public static class MapClass extends Mapper<Text, LongWritable, Text, Text> {
+    public static class MapClass extends Mapper<Text, IntWritable, Text, Text> {
 
         // input: 单词|网页编号->单词在网页编号出现次数
         // output: 网页编号->单词|单词在网页编号出现次数
         @Override
-        protected void map(Text key, LongWritable value, Context context) throws IOException, InterruptedException {
+        protected void map(Text key, IntWritable value, Context context) throws IOException, InterruptedException {
             String keyStr = key.toString();
             String[] fields = keyStr.split(SEPERATOR);
             String word = fields[0];
             String id = fields[1];
+//            System.out.println(id + ": " + word);
             StringBuilder valueBuilder = new StringBuilder();
             context.write(new Text(id), new Text(valueBuilder.append(word).append(SEPERATOR).append(value.get()).toString()));
         }
@@ -53,8 +62,33 @@ public class Mapred2 {
             Set<String> keySet = occursTimes.keySet();
             for (String mapKey : keySet) {
                 double percent = occursTimes.get(mapKey) / wordsCount;
+//                System.out.println(percent);
                 context.write(new Text(mapKey), new DoubleWritable(percent));
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = HBaseConfiguration.create();
+        Job job = Job.getInstance(configuration);
+
+        job.setJarByClass(WordPercentCounter.class);
+        job.setMapperClass(WordPercentCounter.MapClass.class);
+        job.setReducerClass(WordPercentCounter.Reduce.class);
+
+        Path in = new Path("mapred1_output_test/part-r-00000");
+        Path out = new Path("mapred2_output_test");
+        FileSystem.get(configuration).delete(out, true);
+        SequenceFileInputFormat.addInputPath(job, in);
+        SequenceFileOutputFormat.setOutputPath(job, out);
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        job.waitForCompletion(true);
     }
 }
